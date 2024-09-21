09081c13056af8bea4e2a6219d818567bba51f2c
TCarden <timcarden25@gmail.com>
2024-09-21 15:59:48 -0700

    uncommented pages

--- ChubsPlugins/EquipmentCompare/Main.lua
+++ ChubsPlugins/EquipmentCompare/Main.lua
@@ -1,13 +1,13 @@
--- import "ChubsPlugins.EquipmentCompare.Settings";
--- import "ChubsPlugins.EquipmentCompare.ECButton"; -- Import the ECButton class from the other file
--- import "Turbine";
--- import "Turbine.Gameplay";
--- import "Turbine.UI";
--- import "Turbine.UI.Lotro";
+import "ChubsPlugins.EquipmentCompare.Settings";
+import "ChubsPlugins.EquipmentCompare.ECButton"; -- Import the ECButton class from the other file
+import "Turbine";
+import "Turbine.Gameplay";
+import "Turbine.UI";
+import "Turbine.UI.Lotro";
 
--- function debugPrint(text) Turbine.Shell.WriteLine("<rgb=#OOFFFF>DEBUG:</rgb>" ..text);
--- end
+function debugPrint(text) Turbine.Shell.WriteLine("<rgb=#OOFFFF>DEBUG:</rgb>" ..text);
+end
 
--- -- Create and display the ECButton
--- button = ECButton();
--- button:SetVisible(true);
+-- Create and display the ECButton
+button = ECButton();
+button:SetVisible(true);
